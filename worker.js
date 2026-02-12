3c9782e338be5e1a0b2d126220e74765cbb8fee5
magoksroot-sudo <magoksroot@gmail.com>
2026-02-12 17:25:37 +0100

    Update worker.js

--- worker.js
+++ worker.js
@@ -788,8 +788,8 @@ const INDEX = `<!doctype html>
     };
 
     const ONRAMPER_URLS = {
-      EUR: `https://buy.onramper.com/?defaultAmount=49&fiatAmount=49&defaultFiat=EUR&defaultCrypto=USDT_POLYGON&address=${window.ENV_WALLET_ADDRESS}&isAmountEditable=false&isAddressEditable=false`,
-      USD: `https://buy.onramper.com/?defaultAmount=49&fiatAmount=49&defaultFiat=USD&defaultCrypto=USDT_POLYGON&address=${window.ENV_WALLET_ADDRESS}&isAmountEditable=false&isAddressEditable=false`,
+      EUR: `https://buy.onramper.com/?defaultAmount=49&fiatAmount=49&defaultFiat=EUR&defaultCrypto=USDT_POLYGON&address=0xecfdaf07bcb29f3eeb07bafefdff67ca25dffcd5&isAmountEditable=false&isAddressEditable=false`,
+      USD: `https://buy.onramper.com/?defaultAmount=49&fiatAmount=49&defaultFiat=USD&defaultCrypto=USDT_POLYGON&address=0xecfdaf07bcb29f3eeb07bafefdff67ca25dffcd5&isAmountEditable=false&isAddressEditable=false`,
     };
 
     try {
